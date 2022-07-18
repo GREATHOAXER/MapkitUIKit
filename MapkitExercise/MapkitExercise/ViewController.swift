614a9ba4f5ca50815f550e5ff15bd6e2bb6ce4da
한형서 <2017029243@hanyang.ac.kr>
2022-07-18 17:17:18 +0900

    :beers: Create Mapkit view
    Create Mapkit view with storyboard

--- MapkitExercise/MapkitExercise/ViewController.swift
+++ MapkitExercise/MapkitExercise/ViewController.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    //위치와 관련된 매니저 설정
+    @IBOutlet var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
 }
-
