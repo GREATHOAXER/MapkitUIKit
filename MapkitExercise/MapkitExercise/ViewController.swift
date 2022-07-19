c2c8b34cc9b98cb5585f5037b6a66603f44000c9
한형서 <2017029243@hanyang.ac.kr>
2022-07-19 15:34:07 +0900

    :sparkles: Current location focus button
    Append current location focus button and feature

--- MapkitExercise/MapkitExercise/ViewController.swift
+++ MapkitExercise/MapkitExercise/ViewController.swift
@@ -11,14 +11,67 @@ import MapKit
 class ViewController: UIViewController {
     //위치와 관련된 매니저 설정
     @IBOutlet var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    var currentLocation: CLLocationCoordinate2D?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationConfiguration()
         // Do any additional setup after loading the view.
+        
+        
     }
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+
+    private func locationConfiguration(){
+        locationManager.delegate = self
+        let status = locationManager.authorizationStatus
+        if status == .notDetermined{
+            locationManager.requestAlwaysAuthorization()
+        }else if status == .authorizedAlways || status == .authorizedWhenInUse{
+            beginToTrackUserLocation(with: locationManager)
+        }
+    }
+    
+    private func beginToTrackUserLocation(with manager: CLLocationManager){
+        mapView.showsUserLocation = true
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.startUpdatingLocation()
+    }
+    
+    private func zoomToCurrentUserLocation(with coordinates: CLLocationCoordinate2D){
+        let mapRegion = MKCoordinateRegion(center: coordinates, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        mapView.setRegion(mapRegion, animated: false)
+    }
+    
+    @IBAction func findCurrentLocation(_ sender: Any) {
+        let status = locationManager.authorizationStatus
+        if status == .authorizedWhenInUse || status == .authorizedAlways{
+            zoomToCurrentUserLocation(with: currentLocation!)
+        }
+    }
+}
+
+extension ViewController: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+        print("User location Updated")
+        guard let location = locations.first else{ return }
+        if currentLocation == nil{
+            zoomToCurrentUserLocation(with: location.coordinate)
+        }
+        currentLocation = location.coordinate
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager){
+        print("Auth Changed to",manager.authorizationStatus.rawValue)
+        let status = manager.authorizationStatus
+        if status == .authorizedAlways || status == .authorizedWhenInUse{
+            beginToTrackUserLocation(with: manager)
+        }
     }
     
 }
