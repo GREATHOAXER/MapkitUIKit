8871e30f7f7b41416048109eb004027e4c6bb950
한형서 <2017029243@hanyang.ac.kr>
2022-07-20 15:41:25 +0900

    :sparkles: Buttons
    Add weather and location button with shadow property

--- MapkitExercise/MapkitExercise/ViewController.swift
+++ MapkitExercise/MapkitExercise/ViewController.swift
@@ -9,27 +9,40 @@ import UIKit
 import MapKit
 
 class ViewController: UIViewController {
-    //위치와 관련된 매니저 설정
+    //MapView
     @IBOutlet var mapView: MKMapView!
     
-    let locationManager = CLLocationManager()
-    var currentLocation: CLLocationCoordinate2D?
+    //Buttons
+    @IBOutlet var currentLocationButton: UIButton!
+    @IBOutlet var waveInformationButton: UIButton!
     
+    //Location Manager
+    let locationManager = CLLocationManager()
+    
+    //Current Location of User
+    var currentLocation: CLLocationCoordinate2D?
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         locationConfiguration()
         // Do any additional setup after loading the view.
         
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(testForLongPress(gesture:)))
+        longPressGesture.minimumPressDuration = 1
+        mapView.addGestureRecognizer(longPressGesture)
         
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
+    
+    //뷰 생성 후 처음 실행되는 부분
     private func locationConfiguration(){
         locationManager.delegate = self
+        //위치 권한 확인
         let status = locationManager.authorizationStatus
+        //권한이 설정되어 있지 않다면, 설정
         if status == .notDetermined{
             locationManager.requestAlwaysAuthorization()
         }else if status == .authorizedAlways || status == .authorizedWhenInUse{
@@ -37,17 +50,25 @@ class ViewController: UIViewController {
         }
     }
     
+    //사용자의 현재 위치를 받아서 추적하는 메소드
     private func beginToTrackUserLocation(with manager: CLLocationManager){
         mapView.showsUserLocation = true
         manager.desiredAccuracy = kCLLocationAccuracyBest
         manager.startUpdatingLocation()
     }
     
+    //나침판 버튼 누르면, 사용자의 위치로 줌인 되는 메소드
     private func zoomToCurrentUserLocation(with coordinates: CLLocationCoordinate2D){
         let mapRegion = MKCoordinateRegion(center: coordinates, latitudinalMeters: 1000, longitudinalMeters: 1000)
-        mapView.setRegion(mapRegion, animated: false)
+        mapView.setRegion(mapRegion, animated: true)
     }
     
+    //좌표 찾기 위한 테스트 함수
+    @objc func testForLongPress(gesture: UILongPressGestureRecognizer){
+        print("test for longpress")
+    }
+    
+    //나침판 버튼 클릭시에 대한 이벤트 실행
     @IBAction func findCurrentLocation(_ sender: Any) {
         let status = locationManager.authorizationStatus
         if status == .authorizedWhenInUse || status == .authorizedAlways{
@@ -73,5 +94,34 @@ extension ViewController: CLLocationManagerDelegate{
             beginToTrackUserLocation(with: manager)
         }
     }
-    
 }
+
+//setting code for UIView, UIButton shadow property
+@IBDesignable extension UIView {
+    @IBInspectable var shadowRadius: CGFloat {
+        get { return layer.shadowRadius }
+        set { layer.shadowRadius = newValue }
+    }
+
+    @IBInspectable var shadowOpacity: CGFloat {
+        get { return CGFloat(layer.shadowOpacity) }
+        set { layer.shadowOpacity = Float(newValue) }
+    }
+
+    @IBInspectable var shadowOffset: CGSize {
+        get { return layer.shadowOffset }
+        set { layer.shadowOffset = newValue }
+    }
+
+    @IBInspectable var shadowColor: UIColor? {
+        get {
+            guard let cgColor = layer.shadowColor else {
+                return nil
+            }
+            return UIColor(cgColor: cgColor)
+        }
+        set { layer.shadowColor = newValue?.cgColor }
+    }
+}
+
+
